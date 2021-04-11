e364345b0760746089bac8b86edf66b0c79b30d5
Shashank Baghel <theradcolor@gmail.com>
2021-04-11 18:22:53 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=cdb23bba5c48ede5a6163900f8bf423746b70bee, 11042021 build.
    GCC VERSION: aarch64-linux-gnu-gcc (GCC) 11.0.1 20210411 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=cdb23bba5c48ede5a6163900f8bf423746b70bee
    BUILDER COMMIT: 37c5ebb98d4bf5711072f910003e080bad2bf583

--- aarch64-linux-gnu/include/c++/11.0.1/aarch64-linux-gnu/bits/c++config.h
+++ aarch64-linux-gnu/include/c++/11.0.1/aarch64-linux-gnu/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20210409
+#define __GLIBCXX__ 20210411
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
