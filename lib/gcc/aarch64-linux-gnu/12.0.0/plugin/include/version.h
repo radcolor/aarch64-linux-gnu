f230da0e4154e71faaec4e308b3e294974ec734d
Shashank <me@radcolor.dev>
2021-10-19 18:12:12 +0530

    Update to https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=793d2549b173a0a2da6dd20ffc27acb9fd2de73e, 19102021 build.
    GCC VERSION: aarch64-linux-gnu-gcc (GCC) 12.0.0 20211019 (experimental)
    GCC COMMIT URL: https://gcc.gnu.org/git/?p=gcc.git;a=commit;h=793d2549b173a0a2da6dd20ffc27acb9fd2de73e
    BUILDER COMMIT: ba981a1bd26790a676c2095383c1f678cb50a21c

--- lib/gcc/aarch64-linux-gnu/12.0.0/plugin/include/version.h
+++ lib/gcc/aarch64-linux-gnu/12.0.0/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "12.0.0 20211016 (experimental)"
+#define version_string "12.0.0 20211019 (experimental)"
 #define pkgversion_string "(GCC) "
 
 /* This is the location of the online document giving instructions for
